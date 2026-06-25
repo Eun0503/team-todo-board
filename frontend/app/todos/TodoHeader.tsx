50f9ddc0f6d887f98836e2a855a699a3f31b0656
은 <162540155+Eun0503@users.noreply.github.com>
2026-06-26 03:39:01 +0900

    CI test

--- frontend/app/todos/TodoHeader.tsx
+++ frontend/app/todos/TodoHeader.tsx
@@ -1,5 +1,6 @@
 // 투두 플래너 타이틀 헤더 컴포넌트
 export default function TodoHeader() {
+  const fakeNumber: number = "이거 숫자 아닌데 에러 잡아봐라ㅋㅋ";
   return (
     <header className="mb-6">
       <h1 className="text-2xl font-bold text-center text-slate-800 tracking-tight">Todo List</h1>
